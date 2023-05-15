6f5228304eba842a17f0a5c51892b60eaa9a2988
POTN1K <rickeroliver@gmail.com>
2023-05-15 15:47:52 +0200

    Smalle changes to plots

--- model_class.py
+++ model_class.py
@@ -19,7 +19,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pylab import cm
 from tudatpy.kernel.astro import element_conversion
-from Earth_Const import *
+
 
 
 # Constants
@@ -636,53 +636,56 @@ class Model:
         for module in self.modules:
             l += f'{module}\n'
         return l
+    
+    def getSatellites(self):
+        satellites_params = []
+        for module in self.modules:
+            if isinstance(module, Satellite):
+                satellites_params.append(list(module.getParams()))
+        return satellites_params
 
-if __name__ == '__main__':
+if __name__=='__main__':
     # Create model
     model = Model()
 
-    # Add satellite (a, e, i, w, Omega, nu, shift)
-    # model.addSatellite(2.45e7, 0, 58.69, 22.9, 4, 6)
+# Add satellite (a, e, i, w, Omega, nu, shift)
+# model.addSatellite(2.45e7, 0, 58.69, 22.9, 4, 6)
 
-    # Add tower (phi, theta, h)
-    # model.addTower(20, 15, 100000)
+# Add tower (phi, theta, h)
+# model.addTower(20, 15, 100000)
 
-    # Add Lagrange point
-    model.addLagrange('L1')
-    model.addLagrange('L2')
+# Add Lagrange point
+# model.addLagrange('L1')
+# model.addLagrange('L2')
     
-    # Add orbit plane (a, e, i, w, Omega, n_sat, shift)
-    # s = 0
-    #model.addOrbitPlane(2.45e7, 0.5, 85, 0, 0, 4, s)
-    #model.addOrbitPlane(2.45e7, 0.5, 85, 45, 120, 4, s)
-    #model.addOrbitPlane(2.45e7, 0.5, 85, 90, 240, 4, s)
-    #model.addOrbitPlane(2.45e7, 0.5, 0, 0, 0, 4, s)
+# Add orbit plane (a, e, i, w, Omega, n_sat, shift)
+# model.addOrbitPlane(2.45e7, 0.5, 85, 0, 0, 4)
+# model.addOrbitPlane(2.45e7, 0.5, 85, 45, 120, 4)
+# model.addOrbitPlane(2.45e7, 0.5, 85, 90, 240, 4)
+# model.addOrbitPlane(2.45e7, 0.5, 0, 0, 0, 4)
 
-    # Add multiple orbit planes (a, e, i, w, n_planes, n_sat_per_plane, shift, elevation)
-    model.addSymmetricalPlanes(2.45e7, 0, 58.69, 22.9, 6, 4, 90)
+# Add multiple orbit planes (a, e, i, w, n_planes, n_sat_per_plane, shift, elevation)
+# model.addSymmetricalPlanes(2.45e7, 0, 58.69, 22.9, 6, 4, 90)
 
-    # Add fixed point (r, elevation)
-    # model.addFixPoint([2.45e7, 3e6, 1e3], 10)
-    ### Max dist
-    # model.addFixPoint(rc_S3_Mn_max, 10)  # Satellite 3 (S3)
-    # model.addFixPoint(rc_S4_Mn_max, 10)  # S4
-    # model.addFixPoint(rc_S5_Mn_max, 10)  # S5
-    # model.addFixPoint(rc_S6_Mn_max, 10)  # S6
+# Add fixed point (r, elevation)
+# model.addFixPoint([2.45e7, 3e6, 1e3], 10)
 
-    ### Min dist (probably limiting for coverage)
-    #model.addFixPoint(rc_S3_Mn_min, 10)
-    #model.addFixPoint(rc_S4_Mn_min, 10)
-    #model.addFixPoint(rc_S5_Mn_min, 10)
-    #model.addFixPoint(rc_S6_Mn_min, 10)
+# Add Earth coverage
+## Max Distance
+# model.addFixPoint(rc_S3_Mn_max, 10)  # Satellite 3 (S3)
+# model.addFixPoint(rc_S4_Mn_max, 10)  # S4
+# model.addFixPoint(rc_S5_Mn_max, 10)  # S5
+# model.addFixPoint(rc_S6_Mn_max, 10)  # S6
+
+## Min dist (probably limiting for coverage)
+# model.addFixPoint(rc_S3_Mn_min, 10)
+# model.addFixPoint(rc_S4_Mn_min, 10)
+# model.addFixPoint(rc_S5_Mn_min, 10)
+# model.addFixPoint(rc_S6_Mn_min, 10)
 
 
+# Get parameters for satellites in the model
+# model.getParams()
 
-    # Get parameters for satellites in the model
-    # model.getParams()
-
-    print(model)
-
-    # Plot coverage
-    model.plotCoverage()
-
-        
+# Plot coverage
+# model.plotCoverage()
