0a56d450f9275b56019c7692419d05acb351f885
avatarjasper <93929084+avatarjasper@users.noreply.github.com>
2023-05-23 14:05:07 +0200

    Added link budget code and verification code
    Code for calculating the link budget (the huge calculator) and code for verifying its functions. Input values for the validation are included as well. Note that the code is NOT well organized, but this will be changed for the detailed design

---
+++ test_link_budget2.py
@@ -0,0 +1,36 @@
+import unittest
+import link_budget2
+import numpy as np
+
+class Testlink_budget2(unittest.TestCase):
+    def test_Gpeak_helical(self):
+        self.assertAlmostEqual(link_budget2.Gpeak_helical(1, 2, 3), 8.9396, 3)
+    def test_alpha1_over_2_helical(self):
+        self.assertAlmostEqual(link_budget2.alpha1_over_2_helical(1, 2, 3), 60.81635, 3)
+        self.assertEqual(link_budget2.alpha1_over_2_helical(0,1,2), np.inf)
+        self.assertRaises(ZeroDivisionError, link_budget2.alpha1_over_2_helical,1,1,0)
+    def test_Lpr(self):
+        self.assertEqual(link_budget2.Lpr(1,2),-3)
+        self.assertRaises(ZeroDivisionError ,link_budget2.Lpr, 1,0)
+    def test_dB(self):
+        self.assertAlmostEqual(link_budget2.dB(2),3.0103,3)
+        self.assertEqual(link_budget2.dB(0), -np.inf)
+    def test_snr(self):
+        self.assertEqual(link_budget2.snr(1,1,1,1,1,1,1,1,1,1), 4)
+    def test_snr_margin(self):
+        self.assertEqual(link_budget2.snr_margin(1,2), -1)
+    def test_S_lagrange(self):
+        self.assertAlmostEqual(link_budget2.S_lagrange(1,2), 1.7321,3)
+        self.assertEqual(link_budget2.S_lagrange(0,0), 0.0)
+    def test_L_s(self):
+        self.assertAlmostEqual(link_budget2.L_s(1,2), 1.58314e-3, 3)
+        self.assertRaises(ZeroDivisionError, link_budget2.L_s, 1,0)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
