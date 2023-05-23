5c27c349210ee33214744f9e6abaff9272eb5316
ianmaes <94057337+ianmaes@users.noreply.github.com>
2023-05-23 18:28:48 +0200

    Streets of coverage estimation model and V&V of this model

---
+++ streets_of_coverage.py
@@ -0,0 +1,89 @@
+"""
+Script to estimate the number of satellites needed for a Moon constellation and a Lagrange/Moon constellation
+Loop through several options specified by a set of altitudes and number of satellites per orbit
+Number of orbital planes especially is not optimised in this estimation, as the same coverage can be achieved using
+different amount of orbital planes with the satellites distributed over more orbital planes.
+
+By Ian Maes
+"""
+
+import numpy as np
+
+# define radius of the moon
+radius_moon = 1737.4
+# define a set of altitudes
+altitudes = np.array([2500, 5000, 10000, 25000])
+# define a set of number of satellites per orbit that needs to be checked
+S = np.arange(10, 15, 1)
+
+# calculation of inclination and coverage angle for the different altitudes
+def incl(radius, altitude):
+    inclination = np.pi / 2 - (
+                np.pi - np.arcsin((radius * np.sin(100 * np.pi / 180)) / (radius + altitude)) - 100 * np.pi / 180)
+    theta = (np.pi - np.arcsin((radius * np.sin(100 * np.pi / 180)) / (radius + altitude)) - 100 * np.pi / 180)
+    return inclination, theta
+
+
+# calculation of the number of satellites
+def n_sats(theta, S, radius, inclination, lagrange):
+    # calculation of angle of coverage of each satellite
+    between = np.array(np.cos(theta) / np.cos(4 * np.pi / S))
+
+    between[between < 0] = 0
+    c_j = np.arccos(between)
+
+    c_j_lag = np.arccos(np.cos(theta) / np.cos(3 * np.pi / S))
+
+    # Calculation of 1/2 width of coverage of each satellite
+    c = c_j * radius
+    c_lag = c_j_lag * radius
+    # Calculation of the circumference of the moon
+    circ_moon = np.pi * radius
+
+    # Calculation of the number of orbits at different inclinations that are needed
+    n_orbits = np.array(np.ceil(circ_moon / (2 * c / np.sin(inclination))))
+    n_orbits_lag = np.ceil(circ_moon / (2 * c_lag / np.sin(inclination)))
+
+    n_orbits[n_orbits < 2.0] = 2
+
+    # Calculation of the number of satellites needed
+
+    n_sats = n_orbits * S
+    n_sats_lag = n_orbits_lag * S
+
+    if lagrange:
+        n_sats = 1 / 3 * n_sats + 2 / 3 * n_sats_lag
+
+    return n_sats, n_orbits
+
+
+def loop(altitudes, S):
+    inclination_angles = []
+    theta_angles = []
+    j = 0
+    while j < len(altitudes):
+        inclination_angles.append(incl(radius_moon, altitudes[j])[0])
+        theta_angles.append(incl(radius_moon, altitudes[j])[1])
+        j += 1
+
+    inclination_angles = np.array(inclination_angles)
+    print(inclination_angles * 180 / np.pi)
+    inclination_angles = np.full((1, len(inclination_angles)), 90 * np.pi / 180)
+    theta_angles = np.array(theta_angles)
+
+    n_sats_array = np.arange(len(altitudes))
+    n_orbits_array = np.arange(len(altitudes))
+
+    n = 0
+    while n < len(S):
+        n_sats_array = np.vstack((n_sats_array, n_sats(theta_angles, S[n], radius_moon, inclination_angles, True)[0]))
+        n_orbits_array = np.vstack(
+            (n_orbits_array, n_sats(theta_angles, S[n], radius_moon, inclination_angles, True)[1]))
+        n += 1
+    print(n_sats_array)
+    print(n_orbits_array)
+    return n_sats_array, n_orbits_array
+
+
+if __name__ == '__main__':
+    loop(altitudes, S)
---
+++ streets_of_coverage_test.py
@@ -0,0 +1,38 @@
+"""
+Script to run unit tests, system test for the streets of coverage estimation model
+
+By Ian Maes
+"""
+
+import unittest
+import numpy as np
+import streets_of_coverage as ca
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_cov_angle(self):
+        result = ca.incl(1737.4, 1000)[1]
+        print(result)
+        self.assertAlmostEqual(result, 0.721, 3)
+
+    def test_inclination(self):
+        result = ca.incl(1737.4, 1000)[0]
+        print(result)
+        self.assertAlmostEqual(result, 0.850, 2)
+
+    def test_N_orbits(self):
+        result = ca.n_sats(np.pi/4, 20, 1737.4, np.pi / 4, False)[1]
+        self.assertAlmostEqual(result, 5, 2)
+
+    def test_N_sats(self):
+        result = ca.n_sats(np.pi / 4, 20, 1737.4, np.pi / 4, False)[0]
+        self.assertAlmostEqual(result, 100, 2)
+
+    def test_loop_size(self):
+        result = np.size(ca.loop(np.array([3500, 4000, 4500, 40000]), S = np.arange(10, 20, 1))[0])
+        self.assertEqual(result, 44)
+
+    def test_loop_val(self):
+        result = ca.loop(np.array([3500, 4000, 4500, 40000]), S=np.arange(10, 20, 1))[1][3, 0]
+        self.assertEqual(result, 7)
