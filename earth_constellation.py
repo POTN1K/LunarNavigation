194ab300efd5b6bb9e25efaa92ccf0bc7325b146
Serbi12 <serban.nedelcu9@gmail.com>
2023-05-23 17:41:31 +0200

    V&V finished

--- earth_constellation.py
+++ earth_constellation.py
@@ -24,13 +24,13 @@ alpha0 = 0.1732387  # 0.172796400948 min angle for no penetration of SOI for i =
 ir_max = np.arctan2(R_SOI, R_M)  # [rad] => maximum inclination angle of the red orbit
 if np.abs(ir_max*180/np.pi - 9.75694912) > 10**(-9):  # VERIF
     print(ir_max*180/np.pi - 9.75694912)
-ir = 12.7235891*np.pi/180  # 60000118*np.pi/180  # [rad] => actual inclination angle of the red orbit : used as an INPUT
+ir = 12.7235891*np.pi/180  # [rad] => actual inclination angle of the red orbit : used as an INPUT
 ig = - ir  # [rad] => inclination angle of the green orbit
 
 # Time
 t0 = 0
 tf = P_M
-Nb_pts = 400
+Nb_pts = 4
 dt = P_M/(Nb_pts)
 t = np.arange(t0, tf, dt)
 
@@ -203,60 +203,60 @@ Dist_S6_max = np.sqrt(rc_S6_Mn_S4max[0] ** 2 + rc_S6_Mn_S4max[1] ** 2 + rc_S6_Mn
 
 ### Plots
 
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-# ax.scatter(rc_M[np.argmin(DS3), 0], rc_M[np.argmin(DS3), 1], rc_M[np.argmin(DS3), 2], c='r', marker='^')
-# ax.scatter(rc_L1_R[np.argmin(DS3), 0], rc_L1_R[np.argmin(DS3), 1], rc_L1_R[np.argmin(DS3), 2], c='y', marker='o')
-# ax.scatter(rc_L2_R[np.argmin(DS3), 0], rc_L2_R[np.argmin(DS3), 1], rc_L2_R[np.argmin(DS3), 2], c='y', marker='o')
-# ax.scatter(rc_S3_R[np.argmin(DS3), 0], rc_S3_R[np.argmin(DS3), 1], rc_S3_R[np.argmin(DS3), 2], c='b', marker='o')
-# ax.scatter(rc_S4_R[np.argmin(DS3), 0], rc_S4_R[np.argmin(DS3), 1], rc_S4_R[np.argmin(DS3), 2], c='b', marker='o')
-# ax.scatter(rc_S5_R[np.argmin(DS3), 0], rc_S5_R[np.argmin(DS3), 1], rc_S5_R[np.argmin(DS3), 2], c='b', marker='o')
-# ax.scatter(rc_S6_R[np.argmin(DS3), 0], rc_S6_R[np.argmin(DS3), 1], rc_S6_R[np.argmin(DS3), 2], c='b', marker='o')
-#
-# ax.scatter(rc_M[np.argmin(DS4), 0], rc_M[np.argmin(DS4), 1], rc_M[np.argmin(DS4), 2], c='r', marker='^')
-# ax.scatter(rc_L1_R[np.argmin(DS4), 0], rc_L1_R[np.argmin(DS4), 1], rc_L1_R[np.argmin(DS4), 2], c='y', marker='o')
-# ax.scatter(rc_L2_R[np.argmin(DS4), 0], rc_L2_R[np.argmin(DS4), 1], rc_L2_R[np.argmin(DS4), 2], c='y', marker='o')
-# ax.scatter(rc_S3_R[np.argmin(DS4), 0], rc_S3_R[np.argmin(DS4), 1], rc_S3_R[np.argmin(DS4), 2], c='y', marker='o')
-# ax.scatter(rc_S4_R[np.argmin(DS4), 0], rc_S4_R[np.argmin(DS4), 1], rc_S4_R[np.argmin(DS4), 2], c='y', marker='o')
-# ax.scatter(rc_S5_R[np.argmin(DS4), 0], rc_S5_R[np.argmin(DS4), 1], rc_S5_R[np.argmin(DS4), 2], c='y', marker='o')
-# ax.scatter(rc_S6_R[np.argmin(DS4), 0], rc_S6_R[np.argmin(DS4), 1], rc_S6_R[np.argmin(DS4), 2], c='y', marker='o')
-#
-# ax.scatter(rc_M[np.argmax(DS3), 0], rc_M[np.argmax(DS3), 1], rc_M[np.argmax(DS3), 2], c='r', marker='^')
-# ax.scatter(rc_L1_R[np.argmax(DS3), 0], rc_L1_R[np.argmax(DS3), 1], rc_L1_R[np.argmax(DS3), 2], c='y', marker='o')
-# ax.scatter(rc_L2_R[np.argmax(DS3), 0], rc_L2_R[np.argmax(DS3), 1], rc_L2_R[np.argmax(DS3), 2], c='y', marker='o')
-# ax.scatter(rc_S3_R[np.argmax(DS3), 0], rc_S3_R[np.argmax(DS3), 1], rc_S3_R[np.argmax(DS3), 2], c='b', marker='o')
-# ax.scatter(rc_S4_R[np.argmax(DS3), 0], rc_S4_R[np.argmax(DS3), 1], rc_S4_R[np.argmax(DS3), 2], c='b', marker='o')
-# ax.scatter(rc_S5_R[np.argmax(DS3), 0], rc_S5_R[np.argmax(DS3), 1], rc_S5_R[np.argmax(DS3), 2], c='b', marker='o')
-# ax.scatter(rc_S6_R[np.argmax(DS3), 0], rc_S6_R[np.argmax(DS3), 1], rc_S6_R[np.argmax(DS3), 2], c='b', marker='o')
-#
-# ax.scatter(rc_M[np.argmax(DS4), 0], rc_M[np.argmax(DS4), 1], rc_M[np.argmax(DS4), 2], c='r', marker='^')
-# ax.scatter(rc_L1_R[np.argmax(DS4), 0], rc_L1_R[np.argmax(DS4), 1], rc_L1_R[np.argmax(DS4), 2], c='y', marker='o')
-# ax.scatter(rc_L2_R[np.argmax(DS4), 0], rc_L2_R[np.argmax(DS4), 1], rc_L2_R[np.argmax(DS4), 2], c='y', marker='o')
-# ax.scatter(rc_S3_R[np.argmax(DS4), 0], rc_S3_R[np.argmax(DS4), 1], rc_S3_R[np.argmax(DS4), 2], c='y', marker='o')
-# ax.scatter(rc_S4_R[np.argmax(DS4), 0], rc_S4_R[np.argmax(DS4), 1], rc_S4_R[np.argmax(DS4), 2], c='y', marker='o')
-# ax.scatter(rc_S5_R[np.argmax(DS4), 0], rc_S5_R[np.argmax(DS4), 1], rc_S5_R[np.argmax(DS4), 2], c='y', marker='o')
-# ax.scatter(rc_S6_R[np.argmax(DS4), 0], rc_S6_R[np.argmax(DS4), 1], rc_S6_R[np.argmax(DS4), 2], c='y', marker='o')
-#
-for i in range(len(t)):
-   ax.scatter(rc_M[i, 0], rc_M[i, 1], rc_M[i, 2], c='b', marker='o')
-   ax.scatter(rc_L1_R[i, 0], rc_L1_R[i, 1], rc_L1_R[i, 2], c='y', marker='^')
-   ax.scatter(rc_L2_R[i, 0], rc_L2_R[i, 1], rc_L2_R[i, 2], c='y', marker='^')
-for i in range(len(t)):
-   if i in aS :
-       continue
-   else :
-       ax.scatter(rc_S3_R[i, 0], rc_S3_R[i, 1], rc_S3_R[i, 2], c='r', marker='x')
-       ax.scatter(rc_S4_R[i, 0], rc_S4_R[i, 1], rc_S4_R[i, 2], c='r', marker='x')
-       ax.scatter(rc_S5_R[i, 0], rc_S5_R[i, 1], rc_S5_R[i, 2], c='g', marker='x')
-       ax.scatter(rc_S6_R[i, 0], rc_S6_R[i, 1], rc_S6_R[i, 2], c='g', marker='x')
-# # for j in range(len(aS3)):
-# #    ax.scatter(rc_S3_R[aS3[j], 0], rc_S3_R[aS3[j], 1], rc_S3_R[aS3[j], 2], c='r', marker='^')
-# #    ax.scatter(rc_S4_R[aS4[j], 0], rc_S4_R[aS4[j], 1], rc_S4_R[aS4[j], 2], c='r', marker='^')
-# #    ax.scatter(rc_S5_R[aS5[j], 0], rc_S5_R[aS5[j], 1], rc_S5_R[aS5[j], 2], c='r', marker='^')
-# #    ax.scatter(rc_S6_R[aS6[j], 0], rc_S6_R[aS6[j], 1], rc_S6_R[aS6[j], 2], c='r', marker='^')
-# # for k in range(5):
-# #    ax.scatter(rc_S3_R[k, 0], rc_S3_R[k, 1], rc_S3_R[k, 2], c='y', marker='^')
-# #    ax.scatter(rc_S4_R[k, 0], rc_S4_R[k, 1], rc_S4_R[k, 2], c='y', marker='^')
-# #    ax.scatter(rc_S5_R[k, 0], rc_S5_R[k, 1], rc_S5_R[k, 2], c='y', marker='^')
-# #    ax.scatter(rc_S6_R[k, 0], rc_S6_R[k, 1], rc_S6_R[k, 2], c='y', marker='^')
-plt.show()
+# fig = plt.figure()
+# ax = fig.add_subplot(projection='3d')
+# # ax.scatter(rc_M[np.argmin(DS3), 0], rc_M[np.argmin(DS3), 1], rc_M[np.argmin(DS3), 2], c='r', marker='^')
+# # ax.scatter(rc_L1_R[np.argmin(DS3), 0], rc_L1_R[np.argmin(DS3), 1], rc_L1_R[np.argmin(DS3), 2], c='y', marker='o')
+# # ax.scatter(rc_L2_R[np.argmin(DS3), 0], rc_L2_R[np.argmin(DS3), 1], rc_L2_R[np.argmin(DS3), 2], c='y', marker='o')
+# # ax.scatter(rc_S3_R[np.argmin(DS3), 0], rc_S3_R[np.argmin(DS3), 1], rc_S3_R[np.argmin(DS3), 2], c='b', marker='o')
+# # ax.scatter(rc_S4_R[np.argmin(DS3), 0], rc_S4_R[np.argmin(DS3), 1], rc_S4_R[np.argmin(DS3), 2], c='b', marker='o')
+# # ax.scatter(rc_S5_R[np.argmin(DS3), 0], rc_S5_R[np.argmin(DS3), 1], rc_S5_R[np.argmin(DS3), 2], c='b', marker='o')
+# # ax.scatter(rc_S6_R[np.argmin(DS3), 0], rc_S6_R[np.argmin(DS3), 1], rc_S6_R[np.argmin(DS3), 2], c='b', marker='o')
+# #
+# # ax.scatter(rc_M[np.argmin(DS4), 0], rc_M[np.argmin(DS4), 1], rc_M[np.argmin(DS4), 2], c='r', marker='^')
+# # ax.scatter(rc_L1_R[np.argmin(DS4), 0], rc_L1_R[np.argmin(DS4), 1], rc_L1_R[np.argmin(DS4), 2], c='y', marker='o')
+# # ax.scatter(rc_L2_R[np.argmin(DS4), 0], rc_L2_R[np.argmin(DS4), 1], rc_L2_R[np.argmin(DS4), 2], c='y', marker='o')
+# # ax.scatter(rc_S3_R[np.argmin(DS4), 0], rc_S3_R[np.argmin(DS4), 1], rc_S3_R[np.argmin(DS4), 2], c='y', marker='o')
+# # ax.scatter(rc_S4_R[np.argmin(DS4), 0], rc_S4_R[np.argmin(DS4), 1], rc_S4_R[np.argmin(DS4), 2], c='y', marker='o')
+# # ax.scatter(rc_S5_R[np.argmin(DS4), 0], rc_S5_R[np.argmin(DS4), 1], rc_S5_R[np.argmin(DS4), 2], c='y', marker='o')
+# # ax.scatter(rc_S6_R[np.argmin(DS4), 0], rc_S6_R[np.argmin(DS4), 1], rc_S6_R[np.argmin(DS4), 2], c='y', marker='o')
+# #
+# # ax.scatter(rc_M[np.argmax(DS3), 0], rc_M[np.argmax(DS3), 1], rc_M[np.argmax(DS3), 2], c='r', marker='^')
+# # ax.scatter(rc_L1_R[np.argmax(DS3), 0], rc_L1_R[np.argmax(DS3), 1], rc_L1_R[np.argmax(DS3), 2], c='y', marker='o')
+# # ax.scatter(rc_L2_R[np.argmax(DS3), 0], rc_L2_R[np.argmax(DS3), 1], rc_L2_R[np.argmax(DS3), 2], c='y', marker='o')
+# # ax.scatter(rc_S3_R[np.argmax(DS3), 0], rc_S3_R[np.argmax(DS3), 1], rc_S3_R[np.argmax(DS3), 2], c='b', marker='o')
+# # ax.scatter(rc_S4_R[np.argmax(DS3), 0], rc_S4_R[np.argmax(DS3), 1], rc_S4_R[np.argmax(DS3), 2], c='b', marker='o')
+# # ax.scatter(rc_S5_R[np.argmax(DS3), 0], rc_S5_R[np.argmax(DS3), 1], rc_S5_R[np.argmax(DS3), 2], c='b', marker='o')
+# # ax.scatter(rc_S6_R[np.argmax(DS3), 0], rc_S6_R[np.argmax(DS3), 1], rc_S6_R[np.argmax(DS3), 2], c='b', marker='o')
+# #
+# # ax.scatter(rc_M[np.argmax(DS4), 0], rc_M[np.argmax(DS4), 1], rc_M[np.argmax(DS4), 2], c='r', marker='^')
+# # ax.scatter(rc_L1_R[np.argmax(DS4), 0], rc_L1_R[np.argmax(DS4), 1], rc_L1_R[np.argmax(DS4), 2], c='y', marker='o')
+# # ax.scatter(rc_L2_R[np.argmax(DS4), 0], rc_L2_R[np.argmax(DS4), 1], rc_L2_R[np.argmax(DS4), 2], c='y', marker='o')
+# # ax.scatter(rc_S3_R[np.argmax(DS4), 0], rc_S3_R[np.argmax(DS4), 1], rc_S3_R[np.argmax(DS4), 2], c='y', marker='o')
+# # ax.scatter(rc_S4_R[np.argmax(DS4), 0], rc_S4_R[np.argmax(DS4), 1], rc_S4_R[np.argmax(DS4), 2], c='y', marker='o')
+# # ax.scatter(rc_S5_R[np.argmax(DS4), 0], rc_S5_R[np.argmax(DS4), 1], rc_S5_R[np.argmax(DS4), 2], c='y', marker='o')
+# # ax.scatter(rc_S6_R[np.argmax(DS4), 0], rc_S6_R[np.argmax(DS4), 1], rc_S6_R[np.argmax(DS4), 2], c='y', marker='o')
+# #
+# for i in range(len(t)):
+#    ax.scatter(rc_M[i, 0], rc_M[i, 1], rc_M[i, 2], c='b', marker='o')
+#    ax.scatter(rc_L1_R[i, 0], rc_L1_R[i, 1], rc_L1_R[i, 2], c='y', marker='^')
+#    ax.scatter(rc_L2_R[i, 0], rc_L2_R[i, 1], rc_L2_R[i, 2], c='y', marker='^')
+# for i in range(len(t)):
+#    if i in aS :
+#        continue
+#    else :
+#        ax.scatter(rc_S3_R[i, 0], rc_S3_R[i, 1], rc_S3_R[i, 2], c='r', marker='x')
+#        ax.scatter(rc_S4_R[i, 0], rc_S4_R[i, 1], rc_S4_R[i, 2], c='r', marker='x')
+#        ax.scatter(rc_S5_R[i, 0], rc_S5_R[i, 1], rc_S5_R[i, 2], c='g', marker='x')
+#        ax.scatter(rc_S6_R[i, 0], rc_S6_R[i, 1], rc_S6_R[i, 2], c='g', marker='x')
+# # # for j in range(len(aS3)):
+# # #    ax.scatter(rc_S3_R[aS3[j], 0], rc_S3_R[aS3[j], 1], rc_S3_R[aS3[j], 2], c='r', marker='^')
+# # #    ax.scatter(rc_S4_R[aS4[j], 0], rc_S4_R[aS4[j], 1], rc_S4_R[aS4[j], 2], c='r', marker='^')
+# # #    ax.scatter(rc_S5_R[aS5[j], 0], rc_S5_R[aS5[j], 1], rc_S5_R[aS5[j], 2], c='r', marker='^')
+# # #    ax.scatter(rc_S6_R[aS6[j], 0], rc_S6_R[aS6[j], 1], rc_S6_R[aS6[j], 2], c='r', marker='^')
+# # # for k in range(5):
+# # #    ax.scatter(rc_S3_R[k, 0], rc_S3_R[k, 1], rc_S3_R[k, 2], c='y', marker='^')
+# # #    ax.scatter(rc_S4_R[k, 0], rc_S4_R[k, 1], rc_S4_R[k, 2], c='y', marker='^')
+# # #    ax.scatter(rc_S5_R[k, 0], rc_S5_R[k, 1], rc_S5_R[k, 2], c='y', marker='^')
+# # #    ax.scatter(rc_S6_R[k, 0], rc_S6_R[k, 1], rc_S6_R[k, 2], c='y', marker='^')
+# plt.show()
