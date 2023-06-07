5af350b1784feb44adfe02d282b8de6875bebff6
POTN1K <rickeroliver@gmail.com>
2023-06-07 10:37:43 +0200

    Fixed distance calculator ADCS

--- adcs_sub/pointing_accuracy_calculator.py
+++ adcs_sub/pointing_accuracy_calculator.py
@@ -1,80 +1,71 @@
 """"jksa;lda
 By sdasd"""
 
-#Local libraries
+# Local libraries
 from mission_design import OrbitPlane
 import numpy as np
-import csv as csv
+import pandas as pd
 
 r_moon = 1.737e6  # m
 
-with open('Coordinates_in_time.csv', 'r') as file:
+# Functions
+def max_inPlane(plane):
+    """Calculate the relative distance between the satellites in the orbit plane and the x, y and z components of their relative distance.
+    Then, compute the perpendicular distance between the Moon center and relative satellite distance. If it is higher than the Moon radius, then the satellites are in sight, if not, they aren't."""
 
-    reader = csv.reader(file)
-    data = np.array([[float(element) for element in row] for row in reader])
-
-
-
-
-    # print(data)
-    data_no_time = data[:, 1:]
-    # print(data_no_time)
-    positionarray = data_no_time[:, 0:3]
-    for i in range(1, len(data_no_time[0]//6)):
-        positionarray = np.hstack((positionarray, data_no_time[:, 6*i:6*i+3]))
-    positionarray = np.array_split(positionarray, 31)
-    # print(positionarray[0], "Position")
-    south_pole = positionarray[0:2]
-    north_pole = positionarray[2:4]
-    low_incl = positionarray[4:7]
-    og1 = positionarray[7:13]
-    og2 = positionarray[13:19]
-    og3 = positionarray[19:25]
-    og4 = positionarray[25:31]
-
-    # print(south_pole, 'SOuthh')
-
-def Insight(plane):
-
-        QR = []
-        d = []
-        """Calculate the relative distance between the satellites in the orbit plane and the x, y and z components of their relative distance. Then, compute the perpendicular distance between the Moon center and relative satellite distance. If it is higher than the Moon radius, then the satellites are in sight, if not, they aren't."""
-        for i in range(len(plane)):
-            #print(_orbit.satellites[i].r, 'test')
-
-            # QR.append((_orbit.satellites[i].r - _orbit.satellites[(i + 1) % _orbit.n_sat].r))
-            QRi = (plane[i] - plane[(i + 1) % len(plane)])
-            # print(QRi)
-            di = np.sqrt(QRi[0]**2 + QRi[1]**2 + QRi[2]**2)
-            QR.append(QRi)
-            d.append(di)
-        print(np.shape(d))
-        return d, len(d)
-#
-def OutOfPlaneDistance(plane_a, plane_b):
-
-    QP = []
-    min = []
-
-    #print(f'{out_plane_dist=}')
-    for i in range(len(plane_a)):
-        do = np.zeros(len(plane_a[:, 0]), len(plane_a))
-        for j in range(plane_b):
-            QPi = (plane_a[i] - plane_b[j])
-            QP.append(QPi)
-            d = np.sqrt(QPi[0] ** 2 + QPi[1] ** 2 + QPi[2] ** 2)
-            do[:, i] = d
-            for k in range(len(d)):
-                min.append(np.min(do[k, :]))
-#
-#
-#
-#         #print(np.min(out_plane_dist[i]), 'cool')
-#     #print(f'{out_plane_dist=}')
-#     return out_plane_dist, np.min(out_plane_dist)
+    total_d = []
+    for i in range(len(plane)):
+        _ = (plane[i] - plane[(i + 1) % len(plane)])
+        dist = np.linalg.norm(_, axis=1)
+        total_d.append(dist)
+    return np.max(total_d)
 
 
-if __name__=="__main__":
+def max_outPlane(plane_a, plane_b):
+    """Calculate maximum relative distance between two orbit planes"""
+    max_dist = 0
+    for sata in plane_a:
+        tdist = []
+        for satb in plane_b:
+            dist = np.linalg.norm(sata - satb, axis=1)
+            tdist.append(dist)
+        _ = np.max(np.min(tdist, axis=0))
+        if _ > max_dist:
+            max_dist = _
+    return max_dist
+
+
+def clean_csv():
+    df = pd.read_csv('Coordinates_in_time.csv', header=None)
+    # Drop time
+    df.drop(columns=0, inplace=True)
+    # Velocity columns / Drop
+    erase_indices = np.array([[i, i + 1, i + 2] for i in range(4, df.shape[1], 6)]).flatten()
+    df.drop(columns=erase_indices, inplace=True)
+    # Time steps to keep
+    keep = np.arange(3, df.shape[0], 10)
+    clean_df = df.loc[keep]
+    # Separate satellites
+    sat = np.array([clean_df[[i, i + 1, i + 2]].to_numpy() for i in range(1, 93 * 2, 6)])
+    return sat
+
+
+if __name__ == "__main__":
+    # Read csv
+    sat = clean_csv()
+
+    # Planes
+    south_pole = sat[0:2]
+    north_pole = sat[2:4]
+    low_incl = sat[4:7]
+    og1 = sat[7:13]
+    og2 = sat[13:19]
+    og3 = sat[19:25]
+    og4 = sat[25:31]
+
+
+
+
 
     # orb1 = OrbitPlane(a=6541.4e3, e=0.6, i=56.2, w=90, Omega=0, n_sat=2)
     # orb2 = OrbitPlane(a=6541.4e3, e=0.6, i=56.2, w=270, Omega=0, n_sat=2)
@@ -83,9 +74,6 @@ if __name__=="__main__":
     # orb5 = OrbitPlane(a=5701.2e3, e=0.002, i=40.78, w=90, Omega=90, n_sat=6)
     # orb6 = OrbitPlane(a=5701.2e3, e=0.002, i=40.78, w=90, Omega=180, n_sat=6)
     # orb7 = OrbitPlane(a=5701.2e3, e=0.002, i=40.78, w=90, Omega=270, n_sat=6)
-
-
-
 
     # rel1 = orb1.relDistSatellites()
     # rel2 = orb2.relDistSatellites()
@@ -100,14 +88,14 @@ if __name__=="__main__":
     # print(OutOfPlaneDistance(orb2, orb3)[1], "nice")
     # print(OutOfPlaneDistance(orb1, orb3)[1], "sicc")
 
-    #rel2 = orb2.relDistSatellites()
+    # rel2 = orb2.relDistSatellites()
 
-    #sats1 = [sat.r for sat in orb1.satellites]
-    #print(sats1)
+    # sats1 = [sat.r for sat in orb1.satellites]
+    # print(sats1)
 
-    #print(rel1)
+    # print(rel1)
 
-    #print(sats1)
+    # print(sats1)
 
     # def Insight(_orbit):
     #     """Calculate the relative distance between the satellites in the orbit plane and the x, y and z components of their relative distance. Then, compute the perpendicular distance between the Moon center and relative satellite distance. If it is higher than the Moon radius, then the satellites are in sight, if not, they aren't."""
