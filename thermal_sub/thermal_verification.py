ee95cd5164322a7d7921f603c9cc894f9730d8b5
mathijsvereycken <M.N.Vereycken@student.tudelft.nl>
2023-06-21 10:39:10 +0200

    some text for unittests

--- thermal_sub/thermal_verification.py
+++ thermal_sub/thermal_verification.py
@@ -12,36 +12,36 @@ class TestThermal(unittest.TestCase):
         self.assertAlmostEqual(Q, 300.000, places=3)
 
     def test_visability_factor(self):
-        """unittest"""
+        """unittest to check IR correction factor formula"""
         V = th.visibility_factor(1000, 100)
         self.assertAlmostEqual(V, 100.000, places=3)
 
     def test_albedo_radiation(self):
-        """unittest"""
+        """unittest to check albedo factor formula"""
         a = th.albedo_radiation(100, 1)
         self.assertAlmostEqual(a, 100.000, places=3)
 
     def test_intensities(self):
-        """unittest"""
+        """unittest to check intensity formula"""
         I = th.intensities(500, 1000, 2000)
         self.assertAlmostEqual(I, 125.000, places=3)
 
     def test_equilibrium_temperature(self):
-        """unittest"""
+        """unittest to check equilibrium tempereture formula"""
         T = th.equilibrium_temperature(10, 1, 100, 10, 0.1, 0.7, 0.5, 5.67*10**(-8), 100, 0.8)
         self.assertAlmostEqual(T, 159.29030, places=3)
 
     def test_q_in(self):
-        """unittest"""
+        """unittest to check input heat for given temperature formula"""
         q = th.q_in(300, 0.7, 0.5, 5.67*10**(-8), 10, 1, 100, 10, 0.1, 100, 0.8)
         self.assertAlmostEqual(q, 2348.35, places=2)
 
     def test_phase_change(self):
-        """unittest"""
+        """unittest to check mass phase change material formula"""
         m = th.phase_change(500, 1000, 25000, 10, 1000)
         self.assertAlmostEqual(m, 14.2857, places=3)
 
     def test_Q_PC_day(self):
-        """unittest"""
+        """unittest to check heat charging formula"""
         Q_PC = th.Q_PC_day(500, 1000, 10000, 10000, 100000)
         self.assertAlmostEqual(Q_PC, 118.0555, places=3)
